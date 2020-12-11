25f2c817475a8c17cf9ead028415cf1944ab3bbf
n622ryo <fcv53@gmail.com>
2020-12-11 14:39:48 +0900

    新規トレーニング記録投稿のfactrybot作成

--- spec/factories/trainings.rb
+++ spec/factories/trainings.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :training do
-    
+    title               {"腕を太くしたい！"}
+    trainingtime_id     { 2 }
+    part_id             { 4 }
+    event               {"アームカール"}
+    association :user
   end
 end
