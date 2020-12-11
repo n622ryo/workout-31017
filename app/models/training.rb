c74fc090d57435641f4b38858770771aa3a6b5d9
n622ryo <fcv53@gmail.com>
2020-12-11 14:41:30 +0900

    アソシエーションとバリデーション

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,19 +1,22 @@
 class Training < ApplicationRecord
-    #userとのアソシエーション
+  #userとのアソシエーション
   belongs_to :user
+  #commentsとのアソシエーション
   has_many :comments
-    #ActiveHashとのアソシエーション
-    extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :trainingtime
-    belongs_to :part
 
-    with_options presence: true do
-      validates :title
-      validates :event
+  #ActiveHashとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :trainingtime
+  belongs_to :part
 
-      with_options numericality: { other_than: 0 } do
-        validates :trainingtime_id
-        validates :part_id
-      end
+  with_options presence: true do
+    validates :title
+
+    with_options numericality: { other_than: 0 } do
+      validates :trainingtime_id
+      validates :part_id
     end
+
+    validates :event
+  end
 end
