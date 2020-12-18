19e7e78abec8d5b0141d504ca7926edf938ef348
n622ryo <fcv53@gmail.com>
2020-12-18 17:49:18 +0900

    モデル作成(name,password,target))

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
   has_many :trainings
   has_many :comments
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = "ゲスト"
+      user.password = SecureRandom.urlsafe_base64
+      user.target = "目標"
+    end
+  end
+
   with_options presence: true do
     validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
     validates :name
