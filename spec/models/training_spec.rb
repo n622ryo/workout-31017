1611afe34338c711adb53c2815cde7ec8753f709
n622ryo <fcv53@gmail.com>
2020-12-19 23:54:27 +0900

    エラーメッセージを日本語に変更

--- spec/models/training_spec.rb
+++ spec/models/training_spec.rb
@@ -14,33 +14,32 @@ RSpec.describe Training, type: :model do
       it "トレーニングの目的が空だと登録できない" do
         @training.title = nil
         @training.valid?
-        expect(@training.errors.full_messages).to include("Title can't be blank")
+        expect(@training.errors.full_messages).to include("目的を入力してください")
       end
       it "トレーニングの合計時間の情報がないと登録できない" do
         @training.trainingtime_id = nil
         @training.valid?
-        expect(@training.errors.full_messages).to include("Trainingtime can't be blank", "Trainingtime is not a number")
+        expect(@training.errors.full_messages).to include("合計時間を入力してください")
       end
       it "トレーニングの合計時間の選択肢に『----』が選択されている場合は出品できない" do
         @training.trainingtime_id = 0
         @training.valid?
-        expect(@training.errors.full_messages).to include("Trainingtime must be other than 0")
+        expect(@training.errors.full_messages).to include("合計時間は0以外の値にしてください")
       end
       it "トレーニングの部位について情報がないと登録できない" do
         @training.part_id = nil
         @training.valid?
-        expect(@training.errors.full_messages).to include("Part can't be blank", "Part is not a number")
+        expect(@training.errors.full_messages).to include("部位を入力してください")
       end
       it "トレーニングの合計時間の選択肢に『----』が選択されている場合は出品できない" do
         @training.part_id = 0
         @training.valid?
-        binding.pry
-        expect(@training.errors.full_messages).to include("Part must be other than 0")
+        expect(@training.errors.full_messages).to include("部位は0以外の値にしてください")
       end
       it "トレーニングの種目が空だと登録できない" do
         @training.event = nil
         @training.valid?
-        expect(@training.errors.full_messages).to include("Event can't be blank")
+        expect(@training.errors.full_messages).to include("種目を入力してください")
       end
     end
   end
