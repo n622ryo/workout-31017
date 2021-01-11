890132e8b1250adb7c03da5dca52f771c8eeac40
n622ryo <fcv53@gmail.com>
2021-01-12 02:34:28 +0900

    usersコントローラーにedit,update追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
+  end
+
+  def edit
+    redirect_to root_path unless current_user.id == @user.id
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+  end
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :target)
+  end
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
