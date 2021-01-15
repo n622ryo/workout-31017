f7cc189035ab1c6755aaff226c68982db59adb4a
n622ryo <fcv53@gmail.com>
2021-01-15 17:27:39 +0900

    trainingsにネストした形でルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
@@ -8,6 +6,7 @@ Rails.application.routes.draw do
   root to: "trainings#index"
   resources :trainings, only: [:new, :create, :show, :edit, :update, :destroy ] do
     resources :comments, only: :create 
+    resource :favorites, only: [:create, :destroy]
   end
     resources :users, only: [:show, :edit, :update]
 end
