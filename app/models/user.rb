92d273ee7f6ab5210d218cb70437d2afd9b9b889
n622ryo <fcv53@gmail.com>
2021-01-25 15:20:04 +0900

    Userモデルの設定
    フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。
    フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,20 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :favorite_trainings, through: :favorites, source: :training
 
+  # ====================自分がフォローしているユーザーとの関連 ===================================
+  #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。なので親はfollowing_id(フォローする側)
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「followings」と定義
+  has_many :followings, through: :active_relationships, source: :follower
+  # ========================================================================================
+
+  # ====================自分がフォローされるユーザーとの関連 ===================================
+  #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。なので親はfollower_id(フォローされる側)
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # 中間テーブルを介して「following」モデルのUser(フォローする側)を集めることを「followers」と定義
+  has_many :followers, through: :passive_relationships, source: :following
+  # =======================================================================================
+
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
