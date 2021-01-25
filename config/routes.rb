8d28588eca296b3ea13a92b203b941929492eacb
n622ryo <fcv53@gmail.com>
2021-01-25 13:00:08 +0900

    relationdhipsのルーティング作成）
    親であるuserのidが欲しいため、usersの中にネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
+
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+
   root to: "trainings#index"
+
   resources :trainings, only: [:new, :create, :show, :edit, :update, :destroy ] do
     resources :comments, only: :create 
     resource :favorites, only: [:create, :destroy]
   end
-    resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationdhips, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
 end
