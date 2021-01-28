9f20fd9ba414f7affd9c1936e88f3faf17c309f3
n622ryo <fcv53@gmail.com>
2021-01-28 22:24:25 +0900

    メッセージ内容変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to root_path, notice: 'フォローしました'
+    redirect_to root_path, notice: 'フォローしました！'
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to root_path, alert: 'フォローを外しました'
+    redirect_to root_path, alert: 'フォローを解除しました！'
   end
 end
