5a2d7e023cf83ca61c59d1d4a008eccf0655d4ff
n622ryo <fcv53@gmail.com>
2021-03-10 14:08:41 +0900

    エラー解決
    sign_upのpathの時のみ作動するように修正

--- app/javascript/passOpen.js
+++ app/javascript/passOpen.js
@@ -1,17 +1,19 @@
-// 画面が生成されるより先にJavaScriptが走るので、このイベントが必須
-document.addEventListener("DOMContentLoaded", () => {
+if (document.URL.match( /sign_up/ )) {
+  // 画面が生成されるより先にJavaScriptが走るので、このイベントが必須
+  document.addEventListener("DOMContentLoaded", () => {
 
-  const pwd = document.getElementById('js-password');
-  const pwdCheck = document.getElementById('js-passwordCheck');
-  const pwdBtnText = document.getElementById('js-pwdBtnText');
+    const pwd = document.getElementById('js-password');
+    const pwdCheck = document.getElementById('js-passwordCheck');
+    const pwdBtnText = document.getElementById('js-pwdBtnText');
 
-  pwdCheck.addEventListener('change', () => {
-    if(pwdCheck.checked) {
-        pwdBtnText.innerHTML = 'パスワードを隠す'
-          pwd.setAttribute('type', 'text');
-      } else {
-        pwdBtnText.innerHTML = 'パスワードを表示する'
-          pwd.setAttribute('type', 'password');
-      }
-  }, false);
-})
+    pwdCheck.addEventListener('change', () => {
+      if(pwdCheck.checked) {
+          pwdBtnText.innerHTML = 'パスワードを隠す'
+            pwd.setAttribute('type', 'text');
+        } else {
+          pwdBtnText.innerHTML = 'パスワードを表示する'
+            pwd.setAttribute('type', 'password');
+        }
+    }, false);
+  })
+}
